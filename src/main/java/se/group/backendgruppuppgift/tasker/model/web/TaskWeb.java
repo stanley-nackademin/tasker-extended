1b10786fe24f9b36861b5af1c0875caa1d66e407
sinasa2 <sinasa2@yahoo.com>
2018-05-09 10:39:14 +0200

    my branch

--- src/main/java/se/group/backendgruppuppgift/tasker/model/web/TaskWeb.java
+++ src/main/java/se/group/backendgruppuppgift/tasker/model/web/TaskWeb.java
@@ -29,4 +29,6 @@ public final class TaskWeb {
                 name, status
         );
     }
+
+
 }
--- src/main/java/se/group/backendgruppuppgift/tasker/resource/TaskResource.java
+++ src/main/java/se/group/backendgruppuppgift/tasker/resource/TaskResource.java
@@ -2,6 +2,7 @@ package se.group.backendgruppuppgift.tasker.resource;
 
 import org.springframework.stereotype.Component;
 import se.group.backendgruppuppgift.tasker.model.Task;
+import se.group.backendgruppuppgift.tasker.model.TaskStatus;
 import se.group.backendgruppuppgift.tasker.model.web.TaskWeb;
 import se.group.backendgruppuppgift.tasker.service.TaskService;
 
@@ -44,6 +45,22 @@ public final class TaskResource {
     public Response updateTask(@PathParam("id") Long id, TaskWeb task) {
         return service.updateTask(id, task)
                 .map(Response::ok)
+                .orElse(Response.status(NOT_FOUND))
+                .build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response findTask(@PathParam("id") Long id) {
+        return service.findTask(id)
+                .map(Response::ok)
+                .orElse(Response.status(NOT_FOUND))
+                .build();
+    }
+
+    @GET
+    public Response findTaskByStatus(@QueryParam("status") TaskStatus status){
+        return service.findTaskByStatus(status).map(Response::ok)
                 .orElse(Response.status(NOT_FOUND))
                 .build();
     }
