e2af42f3106176796afa35f151de1e8b618b3011
stanley <stanley.ho@yh.nackademin.se>
2018-05-15 13:28:11 +0200

    Removed web model from task service layer

--- src/main/java/se/group/backendgruppuppgift/tasker/resource/TaskResource.java
+++ src/main/java/se/group/backendgruppuppgift/tasker/resource/TaskResource.java
@@ -1,6 +1,8 @@
 package se.group.backendgruppuppgift.tasker.resource;
 
 import org.springframework.stereotype.Component;
+import se.group.backendgruppuppgift.tasker.model.Issue;
+import se.group.backendgruppuppgift.tasker.model.Task;
 import se.group.backendgruppuppgift.tasker.model.web.IssueWeb;
 import se.group.backendgruppuppgift.tasker.model.web.TaskWeb;
 import se.group.backendgruppuppgift.tasker.service.MasterService;
@@ -10,6 +12,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -31,12 +34,13 @@ public final class TaskResource {
     }
 
     @POST
-    public Response createTask(TaskWeb task) {
-        TaskWeb result = service.getTaskService().createTask(task);
+    public Response createTask(TaskWeb taskWeb) {
+        Task result = service.getTaskService().createTask(convertToTaskObject(taskWeb));
+        TaskWeb webResult = convertToWeb(result);
 
         return Response.created(URI.create(uriInfo
                 .getAbsolutePathBuilder()
-                .path(result.getId().toString())
+                .path(webResult.getId().toString())
                 .toString()))
                 .build();
     }
@@ -45,7 +49,7 @@ public final class TaskResource {
     @Path("{id}")
     public Response findTask(@PathParam("id") Long id) {
         return service.getTaskService().findTask(id)
-                .map(Response::ok)
+                .map(t -> Response.ok(convertToWeb(t)))
                 .orElse(Response.status(NOT_FOUND))
                 .build();
     }
@@ -58,23 +62,27 @@ public final class TaskResource {
             @QueryParam("text") String text,
             @QueryParam("issue") String value) {
 
-        return service.getTaskService().findTasksByParams(status, team, user, text, value);
+        List<TaskWeb> result = new ArrayList<>();
+        service.getTaskService().findTasksByParams(status, team, user, text, value)
+                .forEach(t -> result.add(convertToWeb(t)));
+
+        return result;
     }
 
     @PUT
     @Path("{id}")
-    public Response updateTask(@PathParam("id") Long id, TaskWeb task) {
-        return service.getTaskService().updateTask(id, task)
-                .map(Response::ok)
+    public Response updateTask(@PathParam("id") Long id, TaskWeb taskWeb) {
+        return service.getTaskService().updateTask(id, convertToTaskObject(taskWeb))
+                .map(t -> Response.ok(convertToWeb(t)))
                 .orElse(Response.status(NOT_FOUND))
                 .build();
     }
 
     @PUT
     @Path("{id}/issue")
-    public Response assignIssue(@PathParam("id") Long id, IssueWeb issue) {
-        return service.getTaskService().assignIssue(id, issue)
-                .map(Response::ok)
+    public Response assignIssue(@PathParam("id") Long id, IssueWeb issueWeb) {
+        return service.getTaskService().assignIssue(id, convertToIssueObject(issueWeb))
+                .map(t -> Response.ok(convertToWeb(t)))
                 .orElse(Response.status(NOT_FOUND))
                 .build();
     }
@@ -86,5 +94,21 @@ public final class TaskResource {
                 .map(t -> Response.noContent())
                 .orElse(Response.status(NOT_FOUND))
                 .build();
+    }
+
+    private Task convertToTaskObject(TaskWeb taskWeb) {
+        return new Task(taskWeb.getDescription(), taskWeb.getStatus());
+    }
+
+    private Issue convertToIssueObject(IssueWeb issueWeb) {
+        return new Issue(issueWeb.getDescription());
+    }
+
+    private TaskWeb convertToWeb(Task task) {
+        return new TaskWeb(task.getId(), task.getDescription(), task.getStatus(), convertIssueToWeb(task.getIssue()));
+    }
+
+    private IssueWeb convertIssueToWeb(Issue issue) {
+        return issue != null ? new IssueWeb(issue.getDescription(), issue.getIsDone()) : null;
     }
 }
